dae16b9b8b44043a07688d9b288d8b17cee38296
Ismail3427 <ismailurfy@gmail.com>
2026-07-06 09:40:29 -0500

    Update CartController.java

--- src/main/java/com/ctdecomerce/store/cart/controller/CartController.java
+++ src/main/java/com/ctdecomerce/store/cart/controller/CartController.java
@@ -6,6 +6,7 @@ import com.ctdecomerce.store.cart.service.CartService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,7 +20,7 @@ public class CartController {
     }
 
     @PostMapping("/add")
-    public ResponseEntity<CartModel> addToCart(AddToCart add) {
+    public ResponseEntity<CartModel> addToCart(@RequestBody AddToCart add) {
         return new ResponseEntity<>(cartService.addToCart(add), HttpStatus.CREATED);
     }
 }
