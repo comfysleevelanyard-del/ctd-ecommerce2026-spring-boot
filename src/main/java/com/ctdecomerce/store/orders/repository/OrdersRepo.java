0c082ce0cda2617c0aa3e4359ed97b706bf1ae40
Ismail3427 <ismailurfy@gmail.com>
2026-07-08 16:18:14 -0500

    Update OrdersRepo.java

--- src/main/java/com/ctdecomerce/store/orders/repository/OrdersRepo.java
+++ src/main/java/com/ctdecomerce/store/orders/repository/OrdersRepo.java
@@ -1,8 +1,9 @@
 package com.ctdecomerce.store.orders.repository;
 
+import com.ctdecomerce.store.orders.model.OrdersModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
-public interface OrdersRepo extends JpaRepository<OrdersRepo, UUID> {
+public interface OrdersRepo extends JpaRepository<OrdersModel, UUID> {
 }
