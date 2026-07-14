7fb1a601f5e9ebbc99cd77cb6a8864b4814ac602
Ismail3427 <ismailurfy@gmail.com>
2026-07-14 17:19:47 -0500

    add discounts

--- src/main/java/com/ctdecomerce/store/product/service/ProductService.java
+++ src/main/java/com/ctdecomerce/store/product/service/ProductService.java
@@ -75,8 +75,15 @@ public class ProductService {
     }
 
     @Transactional
-    public ProductModel getProductById(IdRequest idRequest) {
-        return productRepo.findById(UUID.fromString(idRequest.getId())).orElse(null);
+    public ProductDTO getProductById(IdRequest idRequest) {
+        ProductModel product = productRepo.findById(UUID.fromString(idRequest.getId())).orElse(null);
+        DiscountsModel discount = discountsRepo.findDiscountsModelByProduct(product);
+        OwnerDTO owner = new OwnerDTO(product.getOwner().getId(), product.getOwner().getName());
+        if (discount == null) {
+            return new ProductDTO(product.getId(), product.getName(), owner, product.getPriceInCents(), false, product.getPriceInCents());
+        } else {
+            return new ProductDTO(product.getId(), product.getName(), owner, (product.getPriceInCents() - product.getPriceInCents() * discount.getOffer()), true, product.getPriceInCents());
+        }
     }
 
     @Transactional
